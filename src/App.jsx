68e6417b458078067976761c482b36e9575058bc
Luong Bao Phuc <ppbao1912@gmail.com>
2025-03-22 22:27:51 +0700

    update postList and delete layoutCenter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/Logo.svg'
 import viteLogo from '/vite.svg'
@@ -8,17 +9,15 @@ import { CreatePost } from "./components/CreatePost";
 import Post from "./components/Post";
 import { LayoutCenter } from "./layout/LayoutCenter";
 >>>>>>> 3e8f5d0 (complete layout center)
+=======
+import { PostList } from "./components/PostList";
+import { Home } from "./pages/Home";
+>>>>>>> e88a3aa (update postList and delete layoutCenter)
 
 function App() {
   return (
     <>
-      <LayoutCenter>
-        <CreatePost />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </LayoutCenter>
+      <Home />
     </>
   );
 }
