ac971c63adde08efab4546352af5f92f6235cd51
prpjzz <groovemusic4399@pm.me>
2025-03-24 05:35:05 +0000

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/Logo.svg'
 import viteLogo from '/vite.svg'
@@ -13,11 +14,19 @@ import { LayoutCenter } from "./layout/LayoutCenter";
 import { PostList } from "./components/PostList";
 import { Home } from "./pages/Home";
 >>>>>>> e88a3aa (update postList and delete layoutCenter)
+=======
+import './App.css'
+import Header from './layout/Header.jsx'
+>>>>>>> f55fee7 (Add Header)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <Home />
+=======
+      <Header />
+>>>>>>> f55fee7 (Add Header)
     </>
   );
 }
